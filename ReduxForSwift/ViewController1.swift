ddd7f220fc742ed76f9796c05cd5b4039200dd52
Hai Pham <swiften.svc@gmail.com>
2018-12-03 00:23:09 +0800

    feat(view-controller-1): hook up state and action for view controller 1

--- ReduxForSwift/ViewController1.swift
+++ ReduxForSwift/ViewController1.swift
@@ -8,4 +8,25 @@
 
 import UIKit
 
-class ViewController1: UIViewController {}
+class ViewController1: UIViewController {
+  @IBOutlet private weak var counterLabel: UILabel!
+  
+  private var subscription: Subscription?
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    self.subscription = Dependency.shared.store.subscribeState("VC1", {
+      self.counterLabel.text = String(describing: $0.counter)
+    })
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    self.subscription?.unsubscribe()
+  }
+  
+  @IBAction func incrementCounter(_ sender: UIButton) {
+    Dependency.shared.store.dispatch(ReduxAction.incrementCounter)
+  }
+}
