08d90b7816b0574ce421b8908e7e9f0901867abc
Hai Pham <swiften.svc@gmail.com>
2018-12-03 00:21:55 +0800

    feat(redux): add action and reducer

---
+++ ReduxForSwift/Redux+Action+Reducer.swift
@@ -0,0 +1,38 @@
+//
+//  Redux+Action+Reducer.swift
+//  ReduxForSwift
+//
+//  Created by Hai Pham on 12/3/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+public struct ReduxState {
+  public let counter: Int
+  
+  public init(counter: Int = 0) {
+    self.counter = counter
+  }
+  
+  public func increment() -> ReduxState {
+    return ReduxState(counter: self.counter + 1)
+  }
+}
+
+public enum ReduxAction: ReduxActionType {
+  case incrementCounter
+}
+
+public final class ReduxReducer {
+  public typealias State = ReduxState
+  
+  public static func reduce(_ state: State, _ action: ReduxActionType) -> State {
+    switch action {
+    case let action as ReduxAction:
+      switch action {
+      case .incrementCounter: return state.increment()
+      }
+      
+    default: return state
+    }
+  }
+}
