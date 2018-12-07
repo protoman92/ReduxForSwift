01e76c43077c76f1944e8d7cfc953ea6064d55bc
Hai Pham <swiften.svc@gmail.com>
2018-12-07 08:03:00 +0800

    feat(view-controller-1): add table view and hook up table cell

--- ReduxForSwift/ViewController1.swift
+++ ReduxForSwift/ViewController1.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class ViewController1: UIViewController {
   @IBOutlet private weak var counterLabel: UILabel!
+  @IBOutlet private weak var tableView: UITableView!
   
   /// Required by prop injector.
   var staticProps: StaticProps?
@@ -22,12 +23,44 @@ class ViewController1: UIViewController {
     }
   }
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.counterLabel.layer.cornerRadius = 4
+    self.counterLabel.layer.borderWidth = 1
+    self.counterLabel.layer.borderColor = UIColor.gray.cgColor
+  }
+  
   func didSetProps(_ props: VariableProps) {
     self.counterLabel.text = String(describing: props.nextState.counter)
+    self.tableView.reloadData()
   }
   
   @IBAction func incrementCounter(_ sender: UIButton) {
     self.variableProps?.action.incrementCounter()
+  }
+}
+
+extension ViewController1: UITableViewDataSource {
+  func tableView(_ tableView: UITableView,
+                 numberOfRowsInSection section: Int) -> Int {
+    return self.variableProps?.nextState.valueCount ?? 0
+  }
+  
+  func tableView(_ tableView: UITableView,
+                 cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView
+      .dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
+      as! TableCell
+    
+    self.staticProps?.injector.injectProps(view: cell, outProps: indexPath.row)
+    return cell
+  }
+}
+
+extension ViewController1: UITableViewDelegate {
+  func tableView(_ tableView: UITableView,
+                 heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 56
   }
 }
 
@@ -37,6 +70,7 @@ extension ViewController1: ReduxCompatibleViewType {
   
   struct StateProps: Equatable {
     let counter: Int
+    let valueCount: Int
   }
   
   struct ActionProps {
@@ -46,7 +80,8 @@ extension ViewController1: ReduxCompatibleViewType {
 
 extension ViewController1: ReduxPropMapperType {
   static func mapState(state: AppState, outProps: OutProps) -> StateProps {
-    return StateProps(counter: state.counter)
+    return StateProps(counter: state.counter,
+                      valueCount: state.valueList.count)
   }
   
   static func mapAction(dispatch: @escaping Redux.Store.Dispatch,
