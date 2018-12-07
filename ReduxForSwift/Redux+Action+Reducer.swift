55e027129e112c0036f3fab6badaca15ed6666c7
Hai Pham <swiften.svc@gmail.com>
2018-12-07 08:01:12 +0800

    feat(redux): add more properties to app state

--- ReduxForSwift/Redux+Action+Reducer.swift
+++ ReduxForSwift/Redux+Action+Reducer.swift
@@ -8,30 +8,50 @@
 
 import ReactiveRedux
 
-public struct AppState {
-  public let counter: Int
+struct AppState {
+  let counter: Int
+  var valueList: [String?]
   
-  public init(counter: Int = 0) {
+  init(counter: Int = 0, valueList: [String?] = ["", "", ""]) {
     self.counter = counter
+    self.valueList = valueList
   }
   
-  public func increment() -> AppState {
+  func increment() -> AppState {
     return AppState(counter: self.counter + 1)
   }
-}
-
-public enum AppAction: ReduxActionType {
-  case incrementCounter
-}
-
-public final class AppReducer {
-  public typealias State = AppState
   
-  public static func reduce(_ state: State, _ action: ReduxActionType) -> State {
+  func updateTextValue(_ index: Int, _ value: String?) -> AppState {
+    var clone = self
+    let length = self.valueList.count
+    
+    if index >= 0 && index < length {
+      clone.valueList[index] = value
+    } else if index >= length {
+      clone.valueList.append(value)
+    }
+    
+    return clone
+  }
+}
+
+enum AppAction: ReduxActionType {
+  case incrementCounter
+  case updateTextValue(Int, String?)
+}
+
+final class AppReducer {
+  typealias State = Dependency.State
+  
+  static func reduce(_ state: State, _ action: ReduxActionType) -> State {
     switch action {
     case let action as AppAction:
       switch action {
-      case .incrementCounter: return state.increment()
+      case .incrementCounter:
+        return state.increment()
+
+      case .updateTextValue(let index, let value):
+        return state.updateTextValue(index, value)
       }
       
     default: return state
