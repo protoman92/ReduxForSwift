8c0fb5c263d5868c4098e7d4c763fc1fba9b8175
Hai Pham <swiften.svc@gmail.com>
2018-12-10 03:06:50 +0800

    feat(view-controller-2): add autocomplete input view, state prop and action

--- ReduxForSwift/ViewController2.swift
+++ ReduxForSwift/ViewController2.swift
@@ -10,24 +10,45 @@ import ReactiveRedux
 import UIKit
 
 final class ViewController2: UIViewController {
+  @IBOutlet private weak var autocompleteInput: UITextField!
+  
   var staticProps: StaticProps?
-  var variableProps: VariableProps?
+  
+  var variableProps: VariableProps? {
+    didSet { self.variableProps.map(self.didSetProps) }
+  }
+  
+  func didSetProps(_ props: VariableProps) {
+    self.autocompleteInput.text = props.nextState.autocompleteInput
+  }
+  
+  @IBAction func updateAutocompleteInput(_ sender: UITextField) {
+    self.variableProps?.action.updateAutocompleteInput(sender.text)
+  }
 }
 
 extension ViewController2: ReduxCompatibleViewType {
   typealias ReduxState = AppState
   typealias OutProps = ()
-  struct StateProps: Equatable {}
-  struct ActionProps {}
+  
+  struct StateProps: Equatable {
+    let autocompleteInput: String?
+  }
+  
+  struct ActionProps {
+    let updateAutocompleteInput: (String?) -> Void
+  }
 }
 
 extension ViewController2: ReduxPropMapperType {
   static func mapState(state: ReduxState, outProps: OutProps) -> StateProps {
-    return StateProps()
+    return StateProps(autocompleteInput: state.autocompleteInput)
   }
   
   static func mapAction(dispatch: @escaping Redux.Store.Dispatch,
                         outProps: OutProps) -> ActionProps {
-    return ActionProps()
+    return ActionProps(
+      updateAutocompleteInput: {dispatch(AppAction.updateAutocompleteInput($0))}
+    )
   }
 }
