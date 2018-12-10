b70e414f917415e5eebdb403a7a9bd91827280a6
Hai Pham <swiften.svc@gmail.com>
2018-12-10 18:58:00 +0800

    feat(repository): add repository for iTunes search

---
+++ ReduxForSwift/Repository.swift
@@ -0,0 +1,35 @@
+//
+//  Repository.swift
+//  ReduxForSwift
+//
+//  Created by Hai Pham on 12/10/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+import Foundation
+
+protocol AppRepositoryType {
+  func searchITunes(_ input: String, _ cb: @escaping (iTunesResult?, Error?) -> Void)
+}
+
+struct AppRepository: AppRepositoryType {
+  private let _api: AppApiType
+  private let _decoder: JSONDecoderType
+  
+  init(_ api: AppApiType, _ decoder: JSONDecoderType) {
+    self._api = api
+    self._decoder = decoder
+  }
+  
+  func searchITunes(_ input: String, _ cb: @escaping (iTunesResult?, Error?) -> Void) {
+    self._api.searchITunes(input) {(d, err) in
+      do {
+        let data = try d.getOrThrow("")
+        let results = try self._decoder.decode(iTunesResult.self, from: data)
+        cb(results, nil)
+      } catch let e {
+        cb(nil, err ?? e)
+      }
+    }
+  }
+}
