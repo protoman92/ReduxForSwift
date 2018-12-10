367e5d2b246906505220851aead761ac12419555
Hai Pham <swiften.svc@gmail.com>
2018-12-10 18:57:04 +0800

    feat(decoder): add JSONDecoderType and make JSONDecoder conform to it

---
+++ ReduxForSwift/JSONDecoder.swift
@@ -0,0 +1,15 @@
+//
+//  JSONDecoder.swift
+//  ReduxForSwift
+//
+//  Created by Hai Pham on 12/10/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+import Foundation
+
+protocol JSONDecoderType {
+  func decode<D>(_ type: D.Type, from data: Data) throws -> D where D: Decodable
+}
+
+extension JSONDecoder: JSONDecoderType {}
