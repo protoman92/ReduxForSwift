732d30278b3d8e227917945d1af90b57629c19f0
Hai Pham <swiften.svc@gmail.com>
2018-12-10 18:55:14 +0800

    feat(data): add iTunes-related data types

---
+++ ReduxForSwift/Data.swift
@@ -0,0 +1,24 @@
+//
+//  Data.swift
+//  ReduxForSwift
+//
+//  Created by Hai Pham on 12/10/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+struct iTunesTrack {
+  let artistName: String
+  let currency: String
+  let trackPrice: Double
+  let trackTimeMillis: Int
+}
+
+struct iTunesResult {
+  let resultCount: Int
+  let results: [iTunesTrack]
+}
+
+extension iTunesTrack: Equatable {}
+extension iTunesTrack: Decodable {}
+extension iTunesResult: Equatable {}
+extension iTunesResult: Decodable {}
